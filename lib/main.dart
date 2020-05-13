5f2b033a77bf1f344c43cf9cad4b3d529ae5ace6
harshchovatiya <harshchovatiya5312@gmail.com>
2020-05-13 10:00:36 +0530

    - Initial commit. - Detect face using firebase ml.

---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+import 'dart:io';
+import 'dart:ui' as ui;
+import 'package:face_detection_demo/services/local_authantic_service.dart';
+import 'package:face_detection_demo/services/service_locator.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  setupLocator();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: MyHomePage(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final LocalAuthenticationService _localAuth =
+      locator<LocalAuthenticationService>();
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     floatingActionButton: FloatingActionButton(
+  //       onPressed: () {},
+  //       child: Icon(Icons.camera_alt),
+  //     ),
+  //     appBar: AppBar(
+  //       title: Text('Face Detection'),
+  //     ),
+  //   );
+  // }
+  File _imageFile;
+  List<Face> _faces;
+  bool isLoading = false;
+  ui.Image _image;
+
+  _getImageAndDetectFaces() async {
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      isLoading = true;
+    });
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final faceDetector = FirebaseVision.instance.faceDetector();
+    List<Face> faces = await faceDetector.processImage(image);
+    if (mounted) {
+      setState(() {
+        _imageFile = imageFile;
+        _faces = faces;
+        _loadImage(imageFile);
+      });
+    }
+  }
+
+  _loadImage(File file) async {
+    final data = await file.readAsBytes();
+    await decodeImageFromList(data).then(
+      (value) => setState(() {
+        _image = value;
+        isLoading = false;
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Face Detection'),
+        actions: <Widget>[
+          GestureDetector(
+            child: Container(
+              margin: EdgeInsets.only(right: 20),
+              child: Icon(Icons.power_settings_new),
+            ),
+            onTap: _localAuth.authenticate,
+          ),
+        ],
+      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : (_imageFile == null)
+              ? Center(child: Text('No image selected'))
+              : Center(
+                  child: FittedBox(
+                    child: SizedBox(
+                      width: _image.width.toDouble(),
+                      height: _image.height.toDouble(),
+                      child: CustomPaint(
+                        painter: FacePainter(_image, _faces),
+                      ),
+                    ),
+                  ),
+                ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getImageAndDetectFaces,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
+
+class FacePainter extends CustomPainter {
+  final ui.Image image;
+  final List<Face> faces;
+  final List<Rect> rects = [];
+
+  FacePainter(this.image, this.faces) {
+    for (var i = 0; i < faces.length; i++) {
+      rects.add(faces[i].boundingBox);
+    }
+  }
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 15.0
+      ..color = Colors.yellow;
+
+    canvas.drawImage(image, Offset.zero, Paint());
+    for (var i = 0; i < faces.length; i++) {
+      canvas.drawRect(rects[i], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(FacePainter oldDelegate) {
+    return image != oldDelegate.image || faces != oldDelegate.faces;
+  }
+}
---
+++ lib/services/service_locator.dart
@@ -0,0 +1,8 @@
+import 'package:face_detection_demo/services/local_authantic_service.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt locator = GetIt();
+
+void setupLocator() {
+  locator.registerLazySingleton(() => LocalAuthenticationService());
+}
